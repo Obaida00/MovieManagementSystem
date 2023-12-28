56ac8ce4d4049179fdcc8b6fb7b665072ddaa6e1
Obaida Almelhem <111151192+Obaida00@users.noreply.github.com>
2023-12-28 22:26:12 +0300

    Revert "Revert "Merge branch 'Ali_90' into Obaida""
    This reverts commit 8d016b54e92d4023d37aea4d2f2ef8dbd52bb382.

--- Programming3Project/src/MainPackage/User.java
+++ Programming3Project/src/MainPackage/User.java
@@ -1,10 +1,13 @@
 package MainPackage;
 
 import Action.go;
+import java.io.Serializable;
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class User {
+public class User implements Serializable{
     private String userName;
     private String userPassword;
     private String userAddress;
@@ -13,6 +16,7 @@ public class User {
     private int userID;//pk
     private static int userIDInitializer;
     private static ArrayList<User> userList ;
+    private static Map<String , Integer> userMap;
 
     public User(String userName, String userPassword,String userAddress){
         this.userName = userName;
@@ -23,8 +27,12 @@ public class User {
         go.saveInitializer("User");
 
         userBookedTickets = new ArrayList<>();
-
+        
         this.image = go.makeImage("Default User Image .jpg");
+        
+        userMap = go.loadUserMap();
+        userMap.put(this.userName , this.userID);
+        go.save("User" , userMap , "userMap");
 
         go.save("User" , this , this.userID);
         userList.add(this);
@@ -44,6 +52,7 @@ public class User {
     public ImageIcon getImage() {return image;}
     public int getUserID() {return userID;}
     public ArrayList<Ticket> getUserBookedTickets() {return userBookedTickets;}
+    public static Map<String, Integer> getUserMap(){return userMap;}
     public static ArrayList<User> getUserList() {return userList;}
     public static int getUserIDInitializer() {return userIDInitializer;}
 }
