7fc951401a728a7ff64e4c83a3469ac7f99c55e0
Obaida Almelhem <111151192+Obaida00@users.noreply.github.com>
2023-12-28 22:25:34 +0300

    Revert "Revert "Revert "merging take 1"""
    This reverts commit c09b9e004cc8cabd9091d40fa62a9f8314ef16e8.

--- Programming3Project/src/MainPackage/Main.java
+++ Programming3Project/src/MainPackage/Main.java
@@ -4,17 +4,8 @@ import Interfaces.LoadingScreen;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import Action.go;
-import Interfaces.LoadingScreen;
-import Interfaces.my_tickets_form;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 
-
+////////////////TESTING
 /**
  *
  * @author Obaida
@@ -22,69 +13,15 @@ import java.util.HashMap;
 public class Main {
 
   public static void main(String[] args) {
-    if(go.loadUserMap() == null){
-        mainInitialize();
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
     }
-   new HomeScreen();
-   my_tickets_form n = new my_tickets_form();
-    
+    LoadingScreen loadingScreen = new LoadingScreen();
+//    new HomeScreen();
   }
-
-
-
-   private static void mainInitialize(){
-        try{
-            File file = new File("Users/UserList.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(new ArrayList<User>());
-            oos.flush();
-            oos.close();
-        }catch(IOException e){
-            System.out.print(e.getMessage());
-        }
-        try{
-            File file = new File("Movies/MovieList.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(new ArrayList<User>());
-            oos.flush();
-            oos.close();
-        }catch(IOException e){
-            System.out.print(e.getMessage());
-        }
-        try{
-            File file = new File("Cinemas/CinemaList.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(new ArrayList<User>());
-            oos.flush();
-            oos.close();
-        }catch(IOException e){
-            System.out.print(e.getMessage());
-        }
-        try{
-            File file = new File("Tickets/TicketList.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(new ArrayList<User>());
-            oos.flush();
-            oos.close();
-        }catch(IOException e){
-            System.out.print(e.getMessage());
-        }
-        //    To Initializing the userMap File By Filling it with empty new HashMap<String , Integer> :
-        try{
-            File file = new File("Users/userMap.txt");
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(new HashMap<String , Integer>());
-            oos.flush();
-            oos.close();
-        }catch(IOException e){
-            System.out.print(e.getMessage());
-        }
-    }
-
 }
-
-
-
 
 
 //    To Print The Date We Did:
--- Programming3Project/src/MainPackage/User.java
+++ Programming3Project/src/MainPackage/User.java
@@ -1,13 +1,10 @@
 package MainPackage;
 
 import Action.go;
-import java.io.Serializable;
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-public class User implements Serializable{
+public class User {
     private String userName;
     private String userPassword;
     private String userAddress;
@@ -16,7 +13,6 @@ public class User implements Serializable{
     private int userID;//pk
     private static int userIDInitializer;
     private static ArrayList<User> userList ;
-    private static Map<String , Integer> userMap;
 
     public User(String userName, String userPassword,String userAddress){
         this.userName = userName;
@@ -27,12 +23,8 @@ public class User implements Serializable{
         go.saveInitializer("User");
 
         userBookedTickets = new ArrayList<>();
-        
+
         this.image = go.makeImage("Default User Image .jpg");
-        
-        userMap = go.loadUserMap();
-        userMap.put(this.userName , this.userID);
-        go.save("User" , userMap , "userMap");
 
         go.save("User" , this , this.userID);
         userList.add(this);
@@ -52,7 +44,6 @@ public class User implements Serializable{
     public ImageIcon getImage() {return image;}
     public int getUserID() {return userID;}
     public ArrayList<Ticket> getUserBookedTickets() {return userBookedTickets;}
-    public static Map<String, Integer> getUserMap(){return userMap;}
     public static ArrayList<User> getUserList() {return userList;}
     public static int getUserIDInitializer() {return userIDInitializer;}
 }
